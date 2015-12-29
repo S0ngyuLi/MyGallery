33e8f2a1d826ca85bdafd97079dc75893bc8ef2b
S0ngyuLi <lisongyu2009@gmail.com>
2015-12-29 21:58:11 +0800

    2.3
    Added small window display for every image

--- move.js
+++ move.js
@@ -1,11 +1,63 @@
-var main = function(){
-    $('.button').click(function(){
-      $('.button').animate({
-      top: "12px"
-    }, 200);
+var index = 1;
+var capacity = 10;
+var small_window = 0;
 
-    });
+var callPage = function(tgt_index){
 
-};
+  if(tgt_index > capacity)
+    return;
 
-$(document).ready(main);
+  if(small_window != 1){
+  //  var flwindow = document.getElementById("floating_window").innerHTML;
+    var new_window = document.createElement("div");
+    new_window.id = "new_window";
+
+    var myImage = document.createElement("img");
+    myImage.id = "myImage";
+    myImage.src = "src/" + tgt_index + ".jpg";
+
+    var miniMenu = document.createElement("div");
+    miniMenu.id = "miniMenu";
+
+    var close_button = document.createElement("button");
+    close_button.id = "close_button";
+
+    close_button.onclick = function(){
+      var tgt_img = document.getElementById("new_window");
+
+      console.log(tgt_img);
+
+      tgt_img.parentNode.removeChild(tgt_img);
+      small_window = 0;
+
+    }
+    var x = document.createTextNode("X");
+
+    close_button.appendChild(x);
+
+    miniMenu.appendChild(close_button);
+
+    new_window.appendChild(myImage);
+
+    new_window.appendChild(miniMenu);
+    document.body.appendChild(new_window);
+    small_window = 1;
+    return;
+  }
+
+  var myImage = document.getElementById("myImage");
+  myImage.src = "src/" + tgt_index + ".jpg";
+
+
+  //"<li id = \"new_window\"> \n" + "<img id = \"myImage\" SRC='src/" + tgt_index + ".jpg'/>" + "<li> <button id = \"close_button\" onclick = \"close()\"> \nx\n </button> </li>" + "\n </li> ";
+  //document.getElementById(webId).innerHTML = "<img id = current";
+}
+
+var close = function(){
+    var tgt_img = document.getElementById("myImage");
+    console.log(tgt_img);
+
+    tgt_img.parentNode.removeChild(tgt_img);
+}
+
+//$(document).ready(main);
