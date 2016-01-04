adf167287b35ee3ee97db3a45f53a8e921b2382e
S0ngyuLi <lisongyu2009@gmail.com>
2016-01-04 10:55:29 +0800

    2.4
    New Functionalities Added

--- move.js
+++ move.js
@@ -12,6 +12,7 @@ var callPage = function(tgt_index){
     return;
 
   if(small_window != 1){
+    small_window = 1;
   //  var flwindow = document.getElementById("floating_window").innerHTML;
     var new_window = document.createElement("div");
     new_window.id = "new_window";
@@ -25,10 +26,12 @@ var callPage = function(tgt_index){
     if(myImage.naturalWidth > w * 0.4){
       myImage.style.width = "40%";
       myImage.style.height = "auto";
+    //  new_window.style.left = "30%";
     }
     if(myImage.naturalHeight > h * 0.5){
       myImage.style.height = "50%";
       myImage.style.width = "auto";
+
     }
 
 
@@ -39,9 +42,7 @@ var callPage = function(tgt_index){
     close_button.id = "close_button";
 
     close_button.onclick = function(){
-      var tgt_img = document.getElementById("new_window");
-
-      if(tgt_img != "undefined"){
+      var tgt_img = document.getElementById("window_back");
         try{
           tgt_img.parentNode.removeChild(tgt_img);
         }
@@ -50,10 +51,16 @@ var callPage = function(tgt_index){
         }
         small_window = 0;
 
-        }
+
       };
 
     var x = document.createTextNode("X");
+
+
+
+    var window_back = document.createElement("div");
+    window_back.id = "window_back";
+
 
     close_button.appendChild(x);
 
@@ -61,17 +68,20 @@ var callPage = function(tgt_index){
     new_window.appendChild(miniMenu);
     new_window.appendChild(myImage);
 
-    document.body.appendChild(new_window);
-    small_window = 1;
+    window_back.appendChild(new_window);
+
+    document.body.appendChild(window_back);
+
     return;
   }
-
-  var myImage = document.getElementById("myImage");
-  myImage.src = "src/" + tgt_index + ".jpg";
-  return;
+  else{
+    var myImage = document.getElementById("myImage");
+    myImage.src = "src/" + tgt_index + ".jpg";
+    return;
+  }
 }
-
-document.body.addEventListener("click", function(){
+//.getElementById("all")
+document.getElementById("all").addEventListener("click", function(){
 
     var tgt_img = document.getElementById("new_window");
     if(small_window !=0){
